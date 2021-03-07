2e766edeb9bd1131c533c3128eab3e9da570e3b1
Dekeniv <64834251+Dekeniv@users.noreply.github.com>
2021-03-07 12:59:42 +0100

    add slider

--- header.inc.php
+++ header.inc.php
@@ -19,16 +19,17 @@
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-
-
-
 
     <!-- Font Awesome 5 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
 
+
+    <!-- Jquery -->
+    <!-- CSS JS EN PREMIER -->
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+    
     <!-- CSS (my style.css always last) -->
     <link rel="stylesheet" href="css/style.css">
 </head>
--- index.php
+++ index.php
@@ -12,6 +12,15 @@
                         <strong class="bold">AlexCraft</strong> est une entreprise spécialisée dans l’<strong class="bold">électricité</strong>, l’<strong class="bold">électronique</strong> et la <strong class="bold">domotique</strong>, domestique ou professionnelle.
                     </h1>
 
+                    <!-- slider -->
+                    <div class="owl-carousel owl-theme slider">
+                        <img src="/img/home.jpg" alt="photo tv">
+                        <img src="/img/smart_home_lights.jpg" alt="photo tv">
+                        <img src="/img/smart_home.jpg" alt="photo tv">
+                        <img src="/img/tv.jpg" alt="photo tv">
+                        <img src="/img/netflix.jpg" alt="photo tv">
+                    </div>
+
                     <p>
                         Avec plus de 10 années d’expérience en entreprise sur des systèmes <strong>électroniques</strong>, <strong>électrotechniques</strong>, et <strong>informatiques</strong>, nous travaillons sur tous types de systèmes, de la Hi-fi à la maison connectée, en passant par l’informatique et la domotique.
                     </p>
@@ -26,13 +35,17 @@
 
                     <ul class="engagements">
                         <li><img src="/img/medal.png" alt="icône de qualité">Qualité premium. <br>
-                            <span>Nous ne travaillons qu’avec des fournisseurs et fabricants reconnus, car dans ces domaines, la qualité et la fiabilité sont incontournables. Les installations fournies sont clé-en-main, vous n’avez rien à faire.</span></li>
+                            <span>Nous ne travaillons qu’avec des fournisseurs et fabricants reconnus, car dans ces domaines, la qualité et la fiabilité sont incontournables. Les installations fournies sont clé-en-main, vous n’avez rien à faire.</span>
+                        </li>
                         <li><img src="/img/file.png" alt="icône de devis">Devis fiables. <br>
-                            <span>Chez <strong>Alexcraft</strong>, aucune mauvaise surprise après le devis. Chaque heure de travail non effectuée sera déduite de la facture. A l’inverse, chaque heure supplémentaire par rapport au devis initial n’est pas facturée.</span></li>
+                            <span>Chez <strong>Alexcraft</strong>, aucune mauvaise surprise après le devis. Chaque heure de travail non effectuée sera déduite de la facture. A l’inverse, chaque heure supplémentaire par rapport au devis initial n’est pas facturée.</span>
+                        </li>
                         <li><img src="/img/clock.png" alt="icône de temps, adaptabilité">Adaptabilité. <br>
-                        <span>Toujours à votre écoute, nous préparons ensemble l’organisation des chantiers, en fonction de vos contraintes personnelles.</span></li>
+                            <span>Toujours à votre écoute, nous préparons ensemble l’organisation des chantiers, en fonction de vos contraintes personnelles.</span>
+                        </li>
                         <li><img src="/img/information.png" alt="icône d'information, communication'">Suivi et communication. <br>
-                        <span>Nous sommes à votre disposition pour vous accompagner au mieux dans vos projets. Nous restons également totalement disponible suite à l’installation, afin de vous aider pas-à-pas dans la prise en main.</span></li>
+                            <span>Nous sommes à votre disposition pour vous accompagner au mieux dans vos projets. Nous restons également totalement disponible suite à l’installation, afin de vous aider pas-à-pas dans la prise en main.</span>
+                        </li>
                     </ul>
 
                 </section>
@@ -45,3 +58,18 @@
 
 <!-- INCLUDE FOOTER -->
 <?php include('footer.inc.php'); ?>
+
+ <!-- scripts -->
+ <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> <!-- jquery cdn en premier-->
+	<script src="js/owl.carousel.min.js"></script> <!-- js local en second -->
+	
+	<!-- js script direct en dernier -->
+	<script>
+	    $(document).ready(function(){
+            $('.owl-carousel').owlCarousel({
+                items:1,
+				dots :true,
+                autoplay :true
+			});
+        });
+	</script>
